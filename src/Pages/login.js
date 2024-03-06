c6b49766a2613a55ae51f874e1e9ad00ef346ca9
Odohi Great <odohigreat@gmail.com>
2024-03-06 15:04:46 -0700

    button redesign

--- src/Pages/login.js
+++ src/Pages/login.js
@@ -5,29 +5,29 @@ import { Link, useNavigate } from 'react-router-dom';
 
 
 export default function () {
-    return (
-        <div>
-            <div>
-                <img className="relative z-40 h-24" src={darkbluecirc} />
-                <img className="absolute top-0 left-0 z-10" src={lightbluecirc} />
-            </div>
-            <div>
-                <h1 className="text-2xl text-center font-bold p-5">Welcome Back!
-                </h1>
-            </div>
-            <div className="m-12">
-                <img className='w-70 h-48' src={tasksdone} />
-            </div>
-            <div className='text-center'>
-                <div className="px-10 mb-52">
-                    <input type='email' placeholder='Enter your email address' className="border-2 rounded-full p-3 w-full"></input><br />
-                    <input type='password' placeholder='Enter your password' className="border-2 rounded-full p-3 w-full my-8"></input><br />
-                </div>
-                <div className='mb-7'>
-                    <Link className="text-white px-32 rounded-lg font-semibold hover:bg-sky-700 text-xl bg-cyan-400 p-5" to="/dashboard">Login</Link>
-                </div>
-                <p>Don't have an account? <Link className="text-sky-500 font-bold" to="/signUp">sign up</Link></p>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<div>
+				<img className="relative z-40 h-24" src={darkbluecirc} />
+				<img className="absolute top-0 left-0 z-10" src={lightbluecirc} />
+			</div>
+			<div>
+				<h1 className="text-2xl text-center font-bold p-5">Welcome Back!
+				</h1>
+			</div>
+			<div className="m-12">
+				<img className='w-70 h-48' src={tasksdone} />
+			</div>
+			<div className='text-center'>
+				<div className="px-10 mb-52">
+					<input type='email' placeholder='Enter your email address' className="border-2 rounded-full p-3 w-full"></input><br />
+					<input type='password' placeholder='Enter your password' className="border-2 rounded-full p-3 w-full my-8"></input><br />
+				</div>
+				<div className='mb-7'>
+					<Link className="cursor-pointer transition-all bg-cyan-400 text-white px-28 py-3 rounded-lg border-cyan-500 border-b-[4px] hover:brightness-110 hover:-translate-y-[1px] hover:border-b-[6px] active:border-b-[2px] active:brightness-90 active:translate-y-[2px]" to="/dashboard">Login</Link>
+				</div>
+				<p>Don't have an account? <Link className="text-sky-500 font-bold" to="/signUp">sign up</Link></p>
+			</div>
+		</div>
+	);
 }
